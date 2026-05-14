62cf811a226a5abcf0b44be28f86293e719f81a8
periyapatnalakshya-png <periyapatnalakshya@gmail.com>
2026-05-14 21:54:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function checkPin(){
+
+  const pin=document.getElementById("pinInput").value;
+
+  if(pin==="143"){
+    document.getElementById("page1").classList.add("hidden");
+    document.getElementById("page2").classList.remove("hidden");
+  }
+
+  else{
+    alert("Wrong PIN 💔");
+  }
+
+}
+
+function openLetter(){
+
+  document.getElementById("page2").classList.add("hidden");
+
+  document.getElementById("page3").classList.remove("hidden");
+
+}
